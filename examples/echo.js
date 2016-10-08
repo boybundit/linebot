4edc81e1f9611f2cff6ddeb55ed52b48be57fb0b
Bundit J <Bundit J>
2016-10-08 22:21:00 +0700

    lib

---
+++ examples/echo.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var linebot = require('../index.js');
+var bot = linebot({
+	channelSecret: process.env.CHANNEL_SECRET,
+	channelAccessToken: process.env.ACCESS_TOKEN
+});
+
+bot.on('message', function (event) {
+	bot.reply(event, event.message.text).then(function (data) {
+		//console.log('OK', data);
+	}).catch(function(error) {
+		//console.log('ERROR', error);
+	});
+});
+
+bot.listen('/linewebhook', process.env.PORT || 80, function () {
+	console.log('LineBot is running.');
+});
