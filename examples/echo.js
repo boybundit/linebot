2496e6f1a24be9bf8c6562c07a38377073671349
Bundit J <Bundit J>
2016-10-09 17:54:59 +0700

    example

--- examples/echo.js
+++ examples/echo.js
@@ -9,17 +9,31 @@ var bot = linebot({
 
 bot.on('message', function (event) {
 	if (event.message.type === 'text') {
+		if (event.message.text === 'image') {
+			return bot.reply(event, {
+				type: 'image',
+				originalContentUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png',
+				previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
+			});
+		}
 		bot.reply(event, event.message.text).then(function (data) {
 			console.log('OK', data);
 		}).catch(function(error) {
 			console.log('ERROR', error);
 		});
 	} else if (event.message.type === 'image') {
-		//bot.reply(event, 'Nice picture!');
-		bot.reply(event, {
-			type: 'image',
-			originalContentUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png',
-    		previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
+		return bot.reply(event, 'Nice picture!');
+	} else if (event.message.type === 'video') {
+		return bot.reply(event, 'Nice movie!');
+	} else if (event.message.type === 'audio') {
+		return bot.reply(event, 'Nice song!');
+	} else if (event.message.type === 'location') {
+		return bot.reply(event, 'That\'s a good location!');
+	} else if (event.message.type === 'sticker') {
+		return bot.reply(event, {
+			type: 'sticker',
+			packageId: 1,
+			stickerId: 1
 		});
 	}
 });
